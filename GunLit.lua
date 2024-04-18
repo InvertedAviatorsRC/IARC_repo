56919105c2dcf277074b94008da2c6d658b9abda
InvertedAviatorsRC <invertedaviatorsrc@gmail.com>
2024-04-18 17:12:50 -0500

    removing test file and adding GunLit file

---
+++ GunLit.lua
@@ -0,0 +1,18 @@
+local inputs = {{ "Input", SOURCE }}
+
+local outputs = { "GunVal" }
+
+local function run(i)
+  
+  if i == 0 then
+    return 1000
+  elseif i == 1000 then
+    return -1000
+  elseif i == -1000 then
+    return 1000
+  end
+    
+end
+
+return { input=inputs, output=outputs, run=run }
+
