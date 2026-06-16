fa5786b328b010bca305d0e79aa392f8af18a88f
InvertedAviatorsRC <166849796+InvertedAviatorsRC@users.noreply.github.com>
2026-06-15 22:23:39 -0500

    Export frame state helpers

--- gps_telemetry_visualizer/__init__.py
+++ gps_telemetry_visualizer/__init__.py
@@ -2,8 +2,13 @@
 
 from gps_telemetry_visualizer.core import (
     RenderConfig,
+    FrameVisualState,
     TelemetryData,
+    compute_frame_states,
     convert_speed,
+    format_distance,
+    format_speed_unit,
+    frame_state_at,
     parse_gps,
     prepare_telemetry,
     render_animation,
@@ -13,8 +18,13 @@ from gps_telemetry_visualizer.core import (
 
 __all__ = [
     "RenderConfig",
+    "FrameVisualState",
     "TelemetryData",
+    "compute_frame_states",
     "convert_speed",
+    "format_distance",
+    "format_speed_unit",
+    "frame_state_at",
     "parse_gps",
     "prepare_telemetry",
     "render_animation",
