5a2e0d375c548a9a378a0dfc14591243ab771247
InvertedAviatorsRC <166849796+InvertedAviatorsRC@users.noreply.github.com>
2026-06-24 23:21:17 -0500

    Use tuned native speedometer text patch

--- native_desktop_app.py
+++ native_desktop_app.py
@@ -1,10 +1,7 @@
-from gps_telemetry_visualizer.native_speedometer_patch import extend_native_app, install
-
-install()
-
+from gps_telemetry_visualizer.native_speedometer_text_patch import setup
 from gps_telemetry_visualizer import native_app
 
-extend_native_app(native_app)
+setup(native_app)
 main = native_app.main
 
 
