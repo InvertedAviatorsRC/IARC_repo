db475a3e713d86d7acb8bbde3ec02227cd1f189e
InvertedAviatorsRC <166849796+InvertedAviatorsRC@users.noreply.github.com>
2026-06-24 22:57:48 -0500

    Install native speedometer variants before launch

--- native_desktop_app.py
+++ native_desktop_app.py
@@ -1,5 +1,9 @@
 from gps_telemetry_visualizer.native_app import main
 
+from gps_telemetry_visualizer.native_speedometer_patch import install
+
+install()
+
 
 if __name__ == "__main__":
     main()
