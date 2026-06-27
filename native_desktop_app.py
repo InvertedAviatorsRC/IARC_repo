a12644232f9b31df23a2d67cf6da3fc647d51e80
InvertedAviatorsRC <166849796+InvertedAviatorsRC@users.noreply.github.com>
2026-06-27 10:10:28 -0500

    Enable signal bars overlay

--- native_desktop_app.py
+++ native_desktop_app.py
@@ -2,11 +2,12 @@ from gps_telemetry_visualizer.native_smoothing_patch import install as install_s
 from gps_telemetry_visualizer.native_speedometer_text_patch import setup
 from gps_telemetry_visualizer.native_colored_trail_patch import setup as setup_colored_trail
 from gps_telemetry_visualizer import native_app
+from gps_telemetry_visualizer import native_signal_health_patch
 
 install_smoothing()
 setup(native_app)
 setup_colored_trail(native_app)
-# signal overlay patch pending
+native_signal_health_patch.setup(native_app)
 main = native_app.main
 
 
