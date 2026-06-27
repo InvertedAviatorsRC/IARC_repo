46193b1f593bd09061e93643ccff195714506dca
InvertedAviatorsRC <166849796+InvertedAviatorsRC@users.noreply.github.com>
2026-06-26 22:25:50 -0500

    Enable continuous telemetry smoothing in native app

--- native_desktop_app.py
+++ native_desktop_app.py
@@ -1,6 +1,8 @@
+from gps_telemetry_visualizer.native_smoothing_patch import install as install_smoothing
 from gps_telemetry_visualizer.native_speedometer_text_patch import setup
 from gps_telemetry_visualizer import native_app
 
+install_smoothing()
 setup(native_app)
 main = native_app.main
 
