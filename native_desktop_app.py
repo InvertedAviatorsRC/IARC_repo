cbe768f5468ddf8fe83c19d8fc0def67630aaacd
InvertedAviatorsRC <166849796+InvertedAviatorsRC@users.noreply.github.com>
2026-06-27 09:56:53 -0500

    Enable speed-colored trail option

--- native_desktop_app.py
+++ native_desktop_app.py
@@ -1,9 +1,11 @@
 from gps_telemetry_visualizer.native_smoothing_patch import install as install_smoothing
 from gps_telemetry_visualizer.native_speedometer_text_patch import setup
+from gps_telemetry_visualizer.native_colored_trail_patch import setup as setup_colored_trail
 from gps_telemetry_visualizer import native_app
 
 install_smoothing()
 setup(native_app)
+setup_colored_trail(native_app)
 main = native_app.main
 
 
